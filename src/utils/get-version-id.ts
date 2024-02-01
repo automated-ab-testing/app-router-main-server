8f68871a9728e93733f316e221789348016ef7e7
rayhankinan <rayhankinan@gmail.com>
2024-02-01 23:09:05 +0700

    Add missing dependencies

--- src/utils/get-version-id.ts
+++ src/utils/get-version-id.ts
@@ -1,3 +1,5 @@
+import { sample } from "lodash";
+
 import { db } from "~/server/db";
 
 export default async function getVersionId() {
@@ -9,4 +11,30 @@ export default async function getVersionId() {
       id: true,
     },
   });
+
+  const randomTest = sample(activeTests);
+
+    if (!randomTest)
+      return {
+        id: null,
+      };
+
+    const versions = await db.version.findMany({
+      where: {
+        testId: randomTest.id,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    // NOTE: Distribusi peluang dapat diubah dengan menggunakan HMM
+    const randomVersion = sample(versions);
+
+    if (!randomVersion)
+      return {
+        id: null,
+      };
+
+    return randomVersion;
 }
