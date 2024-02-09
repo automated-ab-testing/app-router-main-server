3647a4495f2a8b9fe03f25e7445f9ad0c3eed32f
rayhankinan <rayhankinan@gmail.com>
2024-02-09 10:13:17 +0700

    Remove warning data-select

--- src/components/navbar/ThemeSwitcher.tsx
+++ src/components/navbar/ThemeSwitcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Switch } from "@nextui-org/react";
 import { useTheme } from "next-themes";
 
@@ -8,7 +9,13 @@ import MoonIcon from "~/svg/MoonIcon";
 
 export default function ThemeSwitcher() {
   // Get the theme
+  const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
 
   return (
     <Switch
